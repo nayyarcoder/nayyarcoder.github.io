35c673ecb96ef624063bb2f49a1f8cbf0c6775d8
nayyarsan <sivan.satyan@gmail.com>
2025-04-20 08:46:04 +0530

    Add GitHub Actions workflows for deployment and build processes, configure PostCSS and Tailwind CSS, and update project dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.{html,md}',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
